12fa70ec552ed4db3206ed9d8505d73a2da0e3e6
Stanley <>
2020-01-17 18:37:02 +0800

    Added toast extension for Context

--- app/src/main/java/com/stanley/packet_capture/utils/CommonExtensions.kt
+++ app/src/main/java/com/stanley/packet_capture/utils/CommonExtensions.kt
@@ -1,4 +1,9 @@
 package com.stanley.packet_capture.utils
 
+import android.content.Context
+import android.widget.Toast
+
 val Any.TAG: String
     get() = javaClass.simpleName
+
+internal fun Context.toast(msg: String, duration: Int = Toast.LENGTH_SHORT) = Toast.makeText(this, msg, duration).show()
